5a5ce861d800d4fe9ac26dbee7a4272e540e2100
Rainmin Chan <rainminchan@gmail.com>
2017-06-29 17:19:41 +0800

    format codes

--- src/com/rainmin/nautilus/action/LoginApi.java
+++ src/com/rainmin/nautilus/action/LoginApi.java
@@ -14,8 +14,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/login")
 public class LoginApi extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -24,44 +24,44 @@ public class LoginApi extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//doGet(request, response);
-		System.out.println("execute doPost --->");
-		response.setContentType("application/json; charset=UTF-8");
-		response.setCharacterEncoding("UTF-8");
-		PrintWriter out = response.getWriter();
-		
-		String result = "";
-		String username = request.getParameter("username");
-		String password = request.getParameter("pwd");
-		if (username != null && password != null) {
-			System.out.println("username: " + username + ", password: " + password);
-			if (username.equals("rainmin") && password.equals("123456")) {
-				result = "{\"resultCode\":1,\"resultMes\":\"verify through\",\"data\":{\"token\":\"daf32da456hfdh\"}}";
-			} else {
-				result = "{\"resultCode\":2,\"resultMes\":\"username or password is wrong\",\"data\":{\"token\":\"\"}}";
-			}
-		} else {
-			result = "{\"resultCode\":2,\"resultMes\":\"error\",\"token\":\"world\"}";
-			System.out.println("username or password is null");
-		}
-		
-		out.write(result);
-		out.flush();
-		out.close();
-		System.out.println("doPost completed --->");
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        //doGet(request, response);
+        System.out.println("execute doPost --->");
+        response.setContentType("application/json; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        String result = "";
+        String username = request.getParameter("username");
+        String password = request.getParameter("pwd");
+        if (username != null && password != null) {
+            System.out.println("username: " + username + ", password: " + password);
+            if (username.equals("rainmin") && password.equals("123456")) {
+                result = "{\"resultCode\":1,\"resultMes\":\"verify through\",\"data\":{\"token\":\"daf32da456hfdh\"}}";
+            } else {
+                result = "{\"resultCode\":2,\"resultMes\":\"username or password is wrong\",\"data\":{\"token\":\"\"}}";
+            }
+        } else {
+            result = "{\"resultCode\":2,\"resultMes\":\"error\",\"token\":\"world\"}";
+            System.out.println("username or password is null");
+        }
+
+        out.write(result);
+        out.flush();
+        out.close();
+        System.out.println("doPost completed --->");
+    }
 
 }
